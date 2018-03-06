cef525747b3f97ddafc5e198b5ad3cf331becc85
kitscode <aerockevin@gmail.com>
2018-03-06 15:44:40 +0800

    factory -> interface

--- src/org/learn/ioc/BeanDefinition.java
+++ src/org/learn/ioc/BeanDefinition.java
@@ -7,12 +7,41 @@ package org.learn.ioc;
 public class BeanDefinition {
 	
 	private Object bean;
+	
+	private Class beanClass;
 
-	public BeanDefinition(Object bean) {
-		this.bean = bean;
+	private String beanClassName;
+
+	public BeanDefinition() {
+	
+	}
+	
+	public String getBeanClassName() {
+		return beanClassName;
+	}
+
+	public void setBeanClassName(String beanClassName) {
+		this.beanClassName = beanClassName;
+		try {
+			this.beanClass = Class.forName(beanClassName);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Object getBean() {
 		return bean;
 	}
+	
+	public void setBean(Object bean) {
+		this.bean = bean;
+	}
+	public Class getBeanClass() {
+		return beanClass;
+	}
+
+	public void setBeanClass(Class beanClass) {
+		this.beanClass = beanClass;
+	}
+	
 }
--- src/org/learn/test/MyTest.java
+++ src/org/learn/test/MyTest.java
@@ -2,7 +2,8 @@ package org.learn.test;
 
 import org.junit.Test;
 import org.learn.ioc.BeanDefinition;
-import org.learn.ioc.BeanFactory;
+import org.learn.ioc.factory.AutowireCapableBeanFactory;
+import org.learn.ioc.factory.BeanFactory;
 
 /**
  * @author kevin
@@ -12,9 +13,13 @@ public class MyTest {
 	@Test
 	public void testFactory() throws Exception {
 		//initialize bean factory
-		BeanFactory beanFactory=new BeanFactory();
-		//register to beanFactory							     //Definition中包含了真正的实体
-		beanFactory.registerBean("helloService", new BeanDefinition(new HelloSerive()));
+		BeanFactory beanFactory=new AutowireCapableBeanFactory();
+		
+		//register to beanFactory						
+		BeanDefinition beanDefinition=new BeanDefinition();
+		beanDefinition.setBeanClassName("org.learn.test.HelloSerive");
+		beanFactory.registerBeanDefinition("helloService",beanDefinition);
+		
 		//get bean and invoke helloSping method
 		//获得bean都通过bean工厂
 		HelloSerive helloSerive = (HelloSerive) beanFactory.getBean("helloService");
